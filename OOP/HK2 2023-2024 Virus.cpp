c56ab608223f6d2755f1592bedae667122dfa0e5
guy071107 <hle343530@gmail.com>
2026-06-26 16:37:18 +0700

    Update HK2 2023-2024 Virus.cpp

--- OOP/HK2 2023-2024 Virus.cpp
+++ OOP/HK2 2023-2024 Virus.cpp
@@ -1,143 +1,249 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <vector>
 using namespace std;
 
-class ConVat
-{
+class Virus {
     protected:
-        int giaidoan1;
-        int giaidoan2;
-        int giaidoan3;
-        int giaidoan4;
-        string loaidongvat;
+        float ktrchungrate;
+        float trchungnherate;
+        float trchungnangrate;
+        float tuvongrate;
+
+        float tuvongtbrate;
+        float deathrate;
+        float survivalrate;
+
+        bool trieuchungnang = false;
+        bool tuvong = false;
+        bool tiemVaccine = false;
     public:
-        virtual void Nhap() = 0;
-        string getLoaiDongVat()
+    virtual ~Virus() {}
+    virtual void ChoNhiem() = 0;      
+    virtual string TenVirus() = 0;    
+
+    void TaoKhaNangMienDich(int level) {
+        if (!tiemVaccine)
         {
-            return loaidongvat;
+          switch(level) {
+            case 0: // Thấp
+                ktrchungrate   = 0.05f;
+                trchungnherate = 0.15f;
+                trchungnangrate= 0.80f;
+                tuvongrate     = 1.00f;
+                break;
+            case 1: // Trung bình
+                ktrchungrate   = 0.10f;
+                trchungnherate = 0.40f;
+                trchungnangrate= 0.50f;
+                tuvongrate     = 0.70f;
+                break;
+            case 2: // Cao
+                ktrchungrate   = 0.50f;
+                trchungnherate = 0.35f;
+                trchungnangrate= 0.15f;
+                tuvongrate     = 0.50f;
+                break;
+            default:
+                cout<<"Kha nang mien dich khong phu hop!";
+                break;
+            }
         }
-        int getGiaiDoan1()
+        else
         {
-            return giaidoan1;
+          switch(level) {
+            case 0: // Thấp
+                ktrchungrate   = 0.10f;
+                trchungnherate = 0.40f;
+                trchungnangrate= 0.50f;
+                tuvongrate     = 0.80f;
+                break;
+            case 1: // Trung bình
+                ktrchungrate   = 0.20f;
+                trchungnherate = 0.50f;
+                trchungnangrate= 0.30f;
+                tuvongrate     = 0.60f;
+                break;
+            case 2: // Cao
+                ktrchungrate   = 0.70f;
+                trchungnherate = 0.25f;
+                trchungnangrate= 0.05f;
+                tuvongrate     = 0.40f;
+                break;
+            default:
+                cout<<"Kha nang mien dich khong phu hop!";
+                break;
+            }
         }
-        int getGiaiDoan2()
+    }
+
+    void ChoTiemVaccine()
+    {
+        tiemVaccine = true;
+    }
+
+    void CalculateRate() {
+        deathrate     = tuvongrate * tuvongtbrate;
+        survivalrate  = 1 - deathrate;
+        ktrchungrate  *= survivalrate;
+        trchungnherate*= survivalrate;
+        trchungnangrate*= survivalrate;
+    }
+
+    bool TrieuChungNang()
+    {
+        float r = static_cast<float>(rand()) / RAND_MAX;
+        if (r < ktrchungrate + trchungnherate + trchungnangrate && r > ktrchungrate + trchungnherate)
         {
-            return giaidoan2;
+            return true;
         }
-        int getGiaiDoan3()
+        return false;
+    }
+
+    bool TuVong()
+    {
+        float r = static_cast<float>(rand()) / RAND_MAX;
+        if (r < ktrchungrate + trchungnherate + trchungnangrate + tuvongrate && r > ktrchungrate + trchungnherate + trchungnangrate)
         {
-            return giaidoan3;
+            return true;
         }
-        int getGiaiDoan4()
-        {
-            return giaidoan4;
+        return false;
+    }
+
+    void XuatTrieuChung() {
+        float r = static_cast<float>(rand()) / RAND_MAX;
+        cout << "  - r = " << r << "\n";
+        cout << "  Trieu chung virus " << TenVirus() << ": ";
+        if (r < ktrchungrate)
+            cout << "Khong co trieu chung.\n";
+        else if (r < ktrchungrate + trchungnherate)
+            cout << "Trieu chung nhe.\n";
+        else if (r < ktrchungrate + trchungnherate + trchungnangrate)
+            cout << "Trieu chung nang.\n";
+        else
+            cout << "Da tu vong.\n";
+    }
+};
+
+class Covid : public Virus {
+    public:
+        void ChoNhiem() override {
+            tuvongtbrate = 0.05f;
+            CalculateRate();
         }
-        virtual int getGiaidoan5()
-        {
-            return -1;
+        string TenVirus() override { return "Covid"; }
+};
+
+class Ebola : public Virus {
+    public:
+        void ChoNhiem() override {
+            tuvongtbrate = 0.50f;
+            CalculateRate();
+        }
+        string TenVirus() override { return "Ebola"; }
+};
+
+class HIV : public Virus {
+    public:
+        void ChoNhiem() override {
+            tuvongtbrate = 0.90f;
+            CalculateRate();
+        }
+        string TenVirus() override { return "HIV"; }
+};
+
+class VatChu {
+    public:
+        string mavatchu;
+        int khanangmiendich; 
+
+        void Nhap() {
+            cout << "Nhap ma vat chu: ";
+            cin >> mavatchu;
+            cout << "Nhap kha nang mien dich cua vat chu (0.Thap, 1.Trung binh, 2.Cao): ";
+            cin >> khanangmiendich;
         }
 };
 
-class ConMuoi: public ConVat{
-    public:
-        void Nhap()
-        {
-            cout<<"Nhap thong tin vong doi con muoi (ngay): ";
-            cin>>giaidoan1>>giaidoan2>>giaidoan3>>giaidoan4;
-            loaidongvat = "Muoi";
-        }
-};
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
 
-class ConEch: public ConVat{
-    private:
-        int giaidoan5;
-    public:
-        void Nhap()
-        {
-            cout<<"Nhap thong tin vong doi con ech (tuan): ";
-            cin>>giaidoan1>>giaidoan2>>giaidoan3>>giaidoan4>>giaidoan5;
-            loaidongvat = "Ech";
-        }
-        int getGiaidoan5()
-        {
-            return giaidoan5;
-        }
-};
-
-class ConBuom: public ConVat{
-    public:
-        void Nhap()
-        {
-            cout<<"Nhap thong tin vong doi con buom (ngay): ";
-            cin>>giaidoan1>>giaidoan2>>giaidoan3>>giaidoan4;
-            loaidongvat = "Buom";
-        }
-};
-
-int main()
-{
     int n;
-    cout<<"Nhap so luong con vat: ";
+    cout << "Nhap so luong vat chu: ";
+    cin >> n;
+
+    vector<VatChu> vatchuList(n);
+
+    for (int i = 0; i < n; ++i) {
+        cout << "\n=== Vat chu thu " << i+1 << " ===\n";
+        vatchuList[i].Nhap();
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << "\n*** Vat chu " << vatchuList[i].mavatchu
+             << " (mien dich=" << vatchuList[i].khanangmiendich << ") ***\n";
+
+        vector<Virus*> virusList = {
+            new Covid(),
+            new Ebola(),
+            new HIV()
+        };
+
+        for (int j = 0; j < virusList.size(); ++j) {
+            Virus* v = virusList[j];
+            v->TaoKhaNangMienDich(vatchuList[i].khanangmiendich);
+            v->ChoNhiem();
+            v->XuatTrieuChung();
+            delete v;
+        }
+    }
+    
+    vatchuList.clear();
+    cout<<"Nhap so luong vat chu moi: ";
     cin>>n;
-    ConVat* cv = nullptr;
-    vector<ConVat*> animalList;
-    while(n)
-    {
-        int choice;
-        cout<<"Nhap loai dong vat (1. Muoi, 2. Buom, 3. Ech): ";
-        cin>>choice;
-        if (choice == 1)
-        {
-            cv = new ConMuoi();
-        }
-        else if (choice == 2)
-        {
-            cv = new ConBuom();
-        }
-        else if (choice == 3)
-        {
-            cv = new ConEch();
-        }
-        cv->Nhap();
-        animalList.push_back(cv);
-        n--;
+
+    vector<VatChu> vatchuNewList(n);
+
+    int soTrieuChungNang = 0;
+    int soTuVong = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cout << "\n=== Vat chu thu " << i+1 << " ===\n";
+        vatchuNewList[i].Nhap();
     }
 
-    cout<<"Danh sach nhung con vat khong tuan theo quy luat thong thuong: "<<endl;
-    bool coMuoi = false;
-    bool coEch = false;
-    bool coBuom = false;
-    for (int i = 0; i < animalList.size(); i++)
-    {
-        if(animalList[i]->getLoaiDongVat() == "Muoi")
-        {
-            coMuoi = true;
-            if (animalList[i]->getGiaiDoan1() > 3 || (animalList[i]->getGiaiDoan2() < 4 || animalList[i]->getGiaiDoan2() > 10 )|| (animalList[i]->getGiaiDoan3() < 2 || animalList[i]->getGiaiDoan3()> 3) || (animalList[i]->getGiaiDoan4() < 5 || animalList[i]->getGiaiDoan4() > 8))
-            {
-                cout<<"Vi tri cua muoi thu: "<<i<<endl;
-            }
-        }
-        else if (animalList[i]->getLoaiDongVat() == "Ech")
-        {
-            coEch = true;
-            if (animalList[i]->getGiaiDoan1() < 1 || animalList[i]->getGiaiDoan1() > 3 || animalList[i]->getGiaiDoan2() != 4 || animalList[i]->getGiaiDoan3() < 6 || animalList[i]->getGiaiDoan3() > 9 || animalList[i]->getGiaiDoan4() < 1 || animalList[i]->getGiaiDoan4() > 4 || animalList[i]->getGiaidoan5() < 70 || animalList[i]->getGiaidoan5() > 140)
-            {
-                cout<<"Vi tri cua ech thu: "<<i<<endl;
-            }
-        }
-        else if (animalList[i]->getLoaiDongVat() == "Buom")
-        {
-            coBuom = true;
-            if (animalList[i]->getGiaiDoan1() < 3 || animalList[i]->getGiaiDoan1() > 8 || animalList[i]->getGiaiDoan2() < 15 || animalList[i]->getGiaiDoan2() > 16 || animalList[i]->getGiaiDoan3() != 10 || animalList[i]->getGiaiDoan4() < 2 || animalList[i]->getGiaiDoan4() > 3)
-            {
-                cout<<"Vi tri cua buom thu: "<<i<<endl;
-            }
-        }
-    }
+    for (int i = 0; i < n; ++i) {
+        vector<Virus*> virusList = {
+            new Covid(),
+            new Ebola(),
+            new HIV()
+        };
 
-    if (coMuoi && coBuom && coEch)
-    {
-        cout<<"Khu vuc sinh thai co tinh da dang sinh hoc"<<endl;
+        bool daTrieuChungnang = false;
+        bool daTuVong = false;
+
+        for (int j = 0; j < virusList.size(); ++j) {
+            Virus* v = virusList[j];
+            v->ChoTiemVaccine();
+            v->TaoKhaNangMienDich(vatchuNewList[i].khanangmiendich);
+            v->ChoNhiem();
+            v->XuatTrieuChung();
+            if(!daTrieuChungnang && v->TrieuChungNang())
+            {
+                soTrieuChungNang++;
+                daTrieuChungnang = true;
+            }
+            if(!daTuVong && v->TuVong())
+            {
+                soTuVong++;
+                daTuVong = true;
+            }
+        }
     }
+    
+    cout<<"So luong dong vat trieu chung nang: "<<soTrieuChungNang<<endl;
+    cout<<"So luong dong vat da tu vong: "<<soTuVong<<endl;
+
     return 0;
 }
